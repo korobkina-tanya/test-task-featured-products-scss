94c2384712abc758766abf4e4d0a9d1c99550dd4
korobkina-tanya <kor_tanya_88@mail.ru>
2022-01-11 23:22:19 +0300

    changed the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,51 @@
-import gulp from 'gulp';
-import sass from 'gulp-dart-sass';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-import csso from 'postcss-csso';
-import rename from 'gulp-rename';
-import browser from 'browser-sync';
+import gulp from "gulp";
+import sass from "gulp-dart-sass";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import csso from "postcss-csso";
+import rename from "gulp-rename";
+import browser from "browser-sync";
 
-// Styles
-
-export const styles = () => {
-  return gulp.src('source/styles/styles.scss', { sourcemaps: true })
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([
-      autoprefixer(),
-      csso()
-    ]))
-    .pipe(rename('styles.min.css'))
-    .pipe(gulp.dest('build/styles', { sourcemaps: '.' }))
+function styles() {
+  return gulp
+    .src("source/styles/styles.scss", { sourcemaps: true })
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([autoprefixer(), csso()]))
+    .pipe(rename("styles.min.css"))
+    .pipe(gulp.dest("build/styles", { sourcemaps: "." }))
     .pipe(browser.stream());
 }
 
-// HTML
-
-const html = () => {
-  return gulp.src('source/*.html')
-    .pipe(gulp.dest('build'));
+function html() {
+  return gulp.src("source/*.html").pipe(gulp.dest("build"));
 }
 
-// Copy
-
-const copy = () => {
-  return gulp.src('source/img/*.*')
-    .pipe(gulp.dest('build/img'));
+function copy() {
+  return gulp.src("source/img/*.*").pipe(gulp.dest("build/img"));
 }
 
-// Server
-
-const server = (done) => {
+async function server() {
   browser.init({
     server: {
-      baseDir: 'build'
+      baseDir: "build",
     },
     cors: true,
     notify: false,
     ui: false,
   });
-  done();
 }
 
-// Reload
-
-const reload = (done) => {
+async function reload() {
   browser.reload();
-  done();
 }
 
-// Watcher
-
-const watcher = () => {
-  gulp.watch('source/styles/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html', gulp.series(html, reload));
+function watcher() {
+  gulp.watch("source/styles/**/*.scss", gulp.series(styles));
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
-export default gulp.parallel(
-    styles,
-    html,
-    copy,
-    gulp.series(
-      server,
-      watcher
-    )
-  );
+gulp.task(
+  "default",
+  gulp.parallel(styles, html, copy, gulp.series(server, watcher))
+);
+gulp.task("build", gulp.parallel(styles, html, copy));
